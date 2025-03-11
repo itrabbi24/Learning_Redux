993110ec829ffbff1afee62617e29a6ca5a3af4b
itrabbi24 <itrabbi24@gmail.com@itrabbi24>
2025-03-11 11:49:07 +0600

    question show done into card

--- src/home/Question.tsx
+++ src/home/Question.tsx
@@ -1,16 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { useAppSelector } from "@/redux/hooks";
 
 
 export default function Question() {
+
+  const { question, currentQuestionIndex } = useAppSelector(state => state.quiz);
+  const currentQuestion = question[currentQuestionIndex];
+  console.log(currentQuestion);
+
+
   return (
     <>
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardDescription>Question {currentQuestionIndex + 1} Of {question.length} </CardDescription>
+        <CardTitle> {currentQuestion.question} </CardTitle>
       </CardHeader>
       <CardContent>
-
+        {
+          currentQuestion.options.map((option, index) => <Button size={"lg"} className="w-full mt-3" key={index}>{option}</Button>)
+        }
       </CardContent>
       <CardFooter className="flex justify-between">
         Button are here
